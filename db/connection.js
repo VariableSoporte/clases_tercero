91ec1a33ace73c2be58efe310129682bf2bfa790
Luis Fernando Zerna Ramos <101482583+VariableSoporte@users.noreply.github.com>
2026-02-21 04:03:54 +0100

    Update database connection credentials

--- db/connection.js
+++ db/connection.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2');
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'luis1357',
-    database: 'vspolci'
+    password: 'pass',
+    database: 'base'
 });
 
 db.connect((err) => {
